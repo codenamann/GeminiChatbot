5239b57c6623838686dcd5643110489a8569a042
Naman Tiwari <145788363+codenamann@users.noreply.github.com>
2025-11-22 20:22:12 +0530

    new style initialised

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import BackendWakePage from './pages/BackendWakeUp'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BackendWakePage from "./pages/BackendWakeUp";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BackendWakePage />} />
-        <Route path="/chat" element={<App />} />
+        <Route path="/wakeup" element={<BackendWakePage />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
-    
-  </StrictMode>,
-)
+  </StrictMode>
+);
